9d1b8825b02b78c3fd3d1b5b059e20213e47697e
Ronaldo Cesar Pinel <81394682+RonaldoPinel@users.noreply.github.com>
2025-02-03 11:05:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def play_current_track():
 # Função para avançar para a próxima música
 def next_track():
     st.session_state.current_track_index = (st.session_state.current_track_index + 1) % len(tracks)
+    st.experimental_set_query_params(index=st.session_state.current_track_index)
+    st.experimental_rerun()
 
 # Exibir o nome da música atual
 current_track_name = tracks[st.session_state.current_track_index]
@@ -30,4 +32,3 @@ play_current_track()
 # Adicionar o botão para a próxima música
 if st.button("Próxima Música"):
     next_track()
-    st.experimental_rerun()

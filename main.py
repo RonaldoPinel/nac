a4481e4869252276f20acc15b28b34ca8f6ee052
Ronaldo Cesar Pinel <81394682+RonaldoPinel@users.noreply.github.com>
2025-02-03 10:53:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+# Lista de músicas
+tracks = [
+    "1 - Anjos de Resgate.mp3",
+    "2 - Estou Aqui.mp3",
+    "3 - O Céu Está Rezando por Ti.mp3",
+    "4 - Amigos Pela Fé_Mais que Amigos.mp3",
+    "9 - Majestosa Eucaristia _ Invade Minh'alma _ Maria E O Anjo _ Maria da Eucaristia.mp3",
+    "12 - Sou Teu Anjo.mp3",
+    "14 - Mãe da Fé.mp3"
+]
+
+# Adicionar os elementos de áudio como HTML
+audio_elements_html = ''.join([f"<audio id='audio_{i}' src='{track}'></audio>" for i, track in enumerate(tracks)])
+st.markdown(f"<div id='audio-container'>{audio_elements_html}</div>", unsafe_allow_html=True)
+
+# Exibir o player do primeiro áudio
+st.audio(tracks[0], format="audio/mpeg", start_time=0, loop=False)
+
+# Código JavaScript para tocar as músicas em sequência
+st.markdown("""
+<script>
+    const audioElements = Array.from(document.querySelectorAll('audio'));
+    let currentIndex = 0;
+
+    function playNext() {
+        if (currentIndex < audioElements.length) {
+            audioElements[currentIndex].play();
+            audioElements[currentIndex].addEventListener('ended', () => {
+                currentIndex++;
+                playNext();
+            });
+        }
+    }
+
+    const firstAudio = audioElements[currentIndex];
+    firstAudio.addEventListener('ended', playNext);
+</script>
+""", unsafe_allow_html=True)
+
+st.write("As músicas irão tocar em sequência.")
